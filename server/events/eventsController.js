f877ccba3bf718a43f0dd7c67d3bee3503a32384
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-04-19 19:52:23 -0700

    Add mongoose helper functions eventsController.js

---
+++ server/events/eventsController.js
@@ -0,0 +1,32 @@
+var Event = require('./eventsModel.js');
+
+module.exports = {
+  // 
+  addOne: function(req, res) {
+    var newEvent = req.body;
+    Event.create(newEvent, function(err, data) {
+      if (err) {
+        return res.json(err);
+      }
+      res.json(data);
+    });
+  },
+  retrieveOne: function(req, res) {
+    var query = { _id: req.params.id };
+    Event.findOne(query, function(err, data) {
+      if (err) {
+        return res.json(err);
+      }
+      res.json(data);
+    });
+  },
+  retrieveAll: function(req, res) {
+    var query = req.query;
+    Event.find({}, function(err, data) {
+      if (err) {
+        return res.json(err);
+      }
+      res.json(data);
+    });
+  }
+};
