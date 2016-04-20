39212383f4b4cf0bc6c15a8f24f7bd80be89f43e
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-04-20 11:17:50 -0700

    Model schemas updated & relationalized - thanks John!

--- server/restaurants/restaurantModel.js
+++ server/restaurants/restaurantModel.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
+var relationship = require("mongoose-relationship");
 
 // Flesh out our User schema and register the model with Mongoose
 
 var restaurantSchema = new mongoose.Schema({
+  parent: {type: Schema.ObjectId, ref: 'Event', childPath: 'locales'},
   name: {
     type: String,
     unique: true,
@@ -21,6 +23,7 @@ var restaurantSchema = new mongoose.Schema({
   categories: [{type: String}]
 });
 
+restaurantSchema.plugin(relationship, {relationshipPathName: 'parent'});
 var Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
 module.exports = Restaurant;
