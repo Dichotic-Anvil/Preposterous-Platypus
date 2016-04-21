1b25b0f2878c1c51875d3d911c2c2ba8aa294cd9
mrezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-20 23:00:13 -0700

    Comments out uneccesary code and calls proper function to redirect to addToEvent page`

--- client/app/channels/events/events.js
+++ client/app/channels/events/events.js
@@ -1,60 +1,64 @@
 angular.module('platypus.events', [])
 
-.controller('EventsController', function($scope, Likes, Restaurants, Events){
+.controller('EventsController', function($scope, $location, Likes, Restaurants, Events){
 
-  $scope.data = {};
+  // $scope.data = {};
 
-  $scope.data.likes = {};
+  // $scope.data.likes = {};
 
   $scope.toggleForm = false;
 
   $scope.events = [];
 
+  $scope.goToEvent = function (eventId) {
+    $location.path('/addtoevent/'+ eventId);
+  }
 
-  var updateUserLikes = function() {
-    Likes.findUserLikes()
-    .then(function(likes) {
-      $scope.data.likes = {};
-      for (var i = 0; i < likes.length; i++) {
-        $scope.data.likes[likes[i].restaurant] = true;
-      }
-    });
-  };
 
-  updateUserLikes();
+  // var updateUserLikes = function() {
+  //   Likes.findUserLikes()
+  //   .then(function(likes) {
+  //     $scope.data.likes = {};
+  //     for (var i = 0; i < likes.length; i++) {
+  //       $scope.data.likes[likes[i].restaurant] = true;
+  //     }
+  //   });
+  // };
 
-  //get all restaurants from database
-  Restaurants.getAll()
-  .then(function(restaurants) {
-    $scope.data.restaurants = restaurants;
-    console.log("retrievingAll: frontend controller");
-    console.log(restaurants);
-  })
-  .catch(function(error) {
-    console.error(error);
-  });
+  // updateUserLikes();
 
-  $scope.feedOrder = '-likes';
+  // //get all restaurants from database
+  // Restaurants.getAll()
+  // .then(function(restaurants) {
+  //   $scope.data.restaurants = restaurants;
+  //   console.log("retrievingAll: frontend controller");
+  //   console.log(restaurants);
+  // })
+  // .catch(function(error) {
+  //   console.error(error);
+  // });
 
-  //update Likes
-  $scope.updateLikes = function(restID) {
-    Likes.addOrRemove(restID)
-    .then(function(liked) {
-      Restaurants.updateLikes(restID)
-      .then(function(likes) {
-        var restaurants = $scope.data.restaurants;
-        for (var i = 0; i < restaurants.length; i++) {
-          if (restaurants[i]._id === restID) {
-            restaurants[i].likes = likes;
-          }
-        }
-        updateUserLikes();
-      });
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-  };
+  // $scope.feedOrder = '-likes';
+
+  // //update Likes
+  // $scope.updateLikes = function(restID) {
+  //   Likes.addOrRemove(restID)
+  //   .then(function(liked) {
+  //     Restaurants.updateLikes(restID)
+  //     .then(function(likes) {
+  //       var restaurants = $scope.data.restaurants;
+  //       for (var i = 0; i < restaurants.length; i++) {
+  //         if (restaurants[i]._id === restID) {
+  //           restaurants[i].likes = likes;
+  //         }
+  //       }
+  //       updateUserLikes();
+  //     });
+  //   })
+  //   .catch(function(error) {
+  //     console.log(error);
+  //   });
+  // };
 
   $scope.createEvent = function(event) {
     Events.createOne(event)
