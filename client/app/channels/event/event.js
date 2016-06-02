c2baf754e692172187ec408552a882cb3671297a
mrezo <balarezo.m@gmail.com>
2016-06-02 11:07:48 -0700

    Removes old zombie code and fixes indentation

--- client/app/channels/event/event.js
+++ client/app/channels/event/event.js
@@ -11,14 +11,7 @@ angular.module('platypus.event', [])
     for (var i = 0; i < $scope.eventData.event.votes.length; i++) {
       var key = $scope.eventData.event.votes[i].restaurant._id;
       $scope.votes[key] = $scope.eventData.event.votes[i].voters.length;
-      // console.log('added to votes');
-      // $scope.votes.push(++counter);
-
-
-      // $scope.votes.push({ $scope.eventData.event.votes[i].restaurant._id : $scope.eventData.event.votes[i].voters.length });
     }
-    console.log('votes');
-    console.log($scope.votes);
   };
 
 
@@ -30,7 +23,6 @@ angular.module('platypus.event', [])
   Event.getOne(event_id)
     .then(function(event) {
       $scope.eventData.event = event;
-      console.log("Retrieved event successfully", event);
       test();
 
       var arrayOfRestaurants = $scope.eventData.event.options;
@@ -39,18 +31,14 @@ angular.module('platypus.event', [])
         var restaurantId = restaurant._id;
         restaurant.numVotes = restaurant.numVotes || 0;
       }
-      console.log('LOOKIEE HERE');
-      console.log(arrayOfRestaurants);
 
-
-      })
+    })
     .catch(function(err) {
       console.error(err);
     });
 
   $scope.vote = function(restaurant, restaurantId){
     restaurant.numVotes++
-    //SERVER SIDE CODE HERE
 
     Restaurants.updateVotes(restaurantId, restaurant.numVotes)
     .then(function(vote){
@@ -59,18 +47,7 @@ angular.module('platypus.event', [])
     .catch(function(err){
       console.error(err);
     })
-
-    //send a put request to api/events/event_id/vote
-
-  // Event.vote(restaurant._id, event_id)
-  //   .then(function(response) {
-  //     console.log("You placed a vote!", response);
-  //   })
-  //   .catch(function(err) {
-  //     console.error("err", err);
-  //   });
 };
-
 
   $scope.data = {};
   $scope.loading = false;
@@ -92,36 +69,5 @@ angular.module('platypus.event', [])
       });
     });
   };
-
-  $scope.retrieveAll = function(){
-
-  }
-
-  // $scope.addOne = function(restaurant){
-  //   // var restaurantCategories = [];
-
-  //   // // Categories Have extra information that has to be sanitized 
-  //   // for(var i = 0; i < restaurant.categories.length; i++) {
-  //   //   restaurantCategories.push(restaurant.categories[i][0]);
-  //   //}
-
-  //   EventBiz.addOne({
-  //     name: restaurant.name,
-  //     yelpID: restaurant.id,
-  //     eat24_url: restaurant.eat24_url || null,
-  //     image_url: restaurant.image_url,
-  //     upvote: 0,
-  //     // categories: restaurantCategories
-  //   }, function(resp) {
-  //      var id = {restaurant: resp.data._id};
-  //     $location.path('/events/' + event_id);
-  //       // Likes.addOne(id, function(resp) {
-  //       //   Restaurants.updateLikes(id.restaurant) 
-  //       //   .then(function(resp){
-  //       //     console.log('Response to like update', resp);
-  //       //   });
-  //       // });
-  //   });
-  // }
 
  });
